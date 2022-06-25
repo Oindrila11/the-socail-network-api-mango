08c6c489c84629832731f95160265ec1d47f25fe
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-06-25 11:54:00 -0500

    added User model

---
+++ controllers/user-controller.js
@@ -0,0 +1,26 @@
+const {User} = require('../models');
+
+const UserController = {
+    getAllUsers(req, res) {
+        User.find({})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+          console.log(err);
+          res.status(400).json(err);
+        });
+    },
+    getUserById({params},res) {
+        User.findOne({_id:params.id})
+        .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No User found with this id!' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+      },
+    }
