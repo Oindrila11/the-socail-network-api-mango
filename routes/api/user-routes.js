839ab6690e5b3ce22dad59bc31e390d8f658ca39
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-06-25 16:51:56 -0500

    added thought-routes and index

---
+++ routes/api/user-routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  newFriend,
+  deleteFriend,
+
+} = require('../../controllers/user-controller');
+// /api/user
+router
+  .route('/')
+  .get(getAllUser)
+  .post(createUser);
+
+// /api/user/:id
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+// /api/user/:userId/friends/:friendId
+router
+.route(':/userId/friends/:friendId')
+.post(newFriend)
+.delete(deleteFriend);
+
+module.exports = router;
